3d5569e443f01d7f5736d34bc595d91a1121bceb
Abhishek Koirala <readeravskh@gmail.com>
2025-02-15 20:35:40 +0545

    Suspend function overview

--- app/src/main/java/com/developeravsk/coroutinestutorial/MainActivity.kt
+++ app/src/main/java/com/developeravsk/coroutinestutorial/MainActivity.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    val TAG="MainActivity"
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,5 +28,27 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "Hello frm thread ${Thread.currentThread().name}")
         //delay will only pause current coroutine and not block the whole thread
         //if main thread finishes its work , all other threads and coroutines will also be cancelled
+
+        GlobalScope.launch {
+            // delay is a suspend function
+            // suspend function can only be executed within another suspend function or inside of a coroutine
+            // we cannot call delay() inside main thread
+            val networkCall = doNetworkCall()
+            Log.d(TAG, networkCall)
+            val networkcall2 = doNetworkCall2()
+            Log.d(TAG, networkcall2)
+        }
+    }
+
+    // we can also write our own suspend function
+    //but we cannot call the suspend function from outside the coroutine
+    suspend fun doNetworkCall(): String {
+        delay(5000L)
+        return "This is the answer"
+    }
+
+    suspend fun doNetworkCall2(): String {
+        delay(5000L)
+        return "This is the second answer"
     }
 }
