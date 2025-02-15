8e7d0449020680212527700b5bc59470d0f30c04
Abhishek Koirala <readeravskh@gmail.com>
2025-02-15 20:48:32 +0545

    Coroutine contexts

--- app/src/main/java/com/developeravsk/coroutinestutorial/MainActivity.kt
+++ app/src/main/java/com/developeravsk/coroutinestutorial/MainActivity.kt
@@ -2,20 +2,24 @@ package com.developeravsk.coroutinestutorial
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val dummy:TextView=findViewById(R.id.dummy)
         //simples way to start a coroutine
         // globalscope: this corutine will live longer as app lives, but once it finishes its job, it will end
         // coroutines will be started in a separate thread
@@ -38,6 +42,25 @@ class MainActivity : AppCompatActivity() {
             val networkcall2 = doNetworkCall2()
             Log.d(TAG, networkcall2)
         }
+        //dispatchers.MAIN-starts coroutines in main thread, that will be
+        //useful for UI operations from withincoroutines
+        //dispatcher.IO-useful for all kind of data operations, like network call
+        //writing to databases or reading and writing to files
+        //dispatchers.Default-complex and long running calculations to prevent blocking main thread
+        //dispatchers.Unconfined-
+        //we can also start our own new thread by writing newSingleThreadContext("MyThread_name")
+        //useful thing about coroutine context is that we can easily switch them from within a coroutine
+        GlobalScope.launch(Dispatchers.IO) {
+            val answer=doNetworkCall3()
+            //we can switch context now to invoke ui operation in main thread
+            Log.d(TAG,"Starting coroutine in thread ${Thread.currentThread().name}")
+
+            withContext(Dispatchers.Main){
+                //this code in this block will now be executed in mainthread
+                Log.d(TAG,"Setting text in thread ${Thread.currentThread().name}")
+                dummy.text=answer
+            }
+        }
     }
 
     // we can also write our own suspend function
@@ -45,6 +68,10 @@ class MainActivity : AppCompatActivity() {
     suspend fun doNetworkCall(): String {
         delay(5000L)
         return "This is the answer"
+    }
+    suspend fun doNetworkCall3(): String {
+        delay(5000L)
+        return "This is the context answer"
     }
 
     suspend fun doNetworkCall2(): String {
